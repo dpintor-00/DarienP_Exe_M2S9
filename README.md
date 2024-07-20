b526eb9f86c3fa41bcfe6fb699560a22a169c9b6
Darien Pinto <pintodarien@gmail.com>
2024-07-20 17:03:55 -0400

    Ejercicio 9 Listo

---
+++ README.md
@@ -0,0 +1,4 @@
+Este repo contiene el desarrollo del noveno ejercicio del módulo 2 del bootcamp.  
+  
+Para descargar por favor pegue en su terminal:  
+git clone https://github.com/dpintor-00/DarienP_Exe_M2S9.git  
---
+++ index.js
@@ -0,0 +1,24 @@
+function mostrarTexto2(){
+    document.getElementById("text2").style.display = "block";
+}
+
+function ocultarTexto2(){
+    document.getElementById("text2").style.display = "none";
+}
+
+
+function agrandarImg(){
+    cajaGrande = document.getElementById("caja2").style.width = "100%";
+}
+
+function retraerImg(){
+    cajaNormal = document.getElementById("caja2").style.width = "20%";
+}
+
+function agrandarTexto(){
+    textoGigante = document.getElementById("caja3").style.fontSize = "xx-large";
+}
+
+function achicarTexto(){
+    textoNormal = document.getElementById("caja3").style.fontSize = "medium";
+}
